4446adc2dfe6e20afa6962726f00b5738c3babd0
Tim van Eijndhoven <tvaneijndhoven@jpoint.nl>
2017-02-16 14:18:19 +0100

    Remove ChatClient from the mix

--- vertx-chat/vertx-chat-web/src/main/java/nl/jpoint/vertx/chat/web/WebChatClient.java
+++ vertx-chat/vertx-chat-web/src/main/java/nl/jpoint/vertx/chat/web/WebChatClient.java
@@ -1,8 +1,8 @@
 package nl.jpoint.vertx.chat.web;
 
-import nl.jpoint.vertx.chat.client.ChatClient;
+import nl.jpoint.vertx.chat.api.AbstractChatClient;
 
-public class WebChatClient extends ChatClient {
+public class WebChatClient extends AbstractChatClient {
 
     public WebChatClient(String handle) {
         super(handle);
@@ -10,11 +10,9 @@ public class WebChatClient extends ChatClient {
 
     @Override
     public void start() throws Exception {
-        super.start();
+        // TODO: Retrieve key from shared data and create a token using createToken() (inherited from the super class).
 
-        // TODO: Retrieve key from shared data and store it in instance variable 'token' (inherited from the super class)
-
-        // TODO: start sending some chat messages to the chat endpoint. Hint: look at the helper methods in the super class
+        // TODO: start sending some chat messages to the chat endpoint (address is provided by CHANNEL in the superclass).
     }
 }
 
